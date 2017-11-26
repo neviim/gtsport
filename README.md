4ab9ea7f9e04a909b7a012d9c9804c96e449bdb4
Jorge Silva <neviim@Mac-mini-de-Jorge.local>
2017-11-26 20:30:27 -0200

    Modulo de cadastro no banco e controle de duplicidade.

--- README.md
+++ README.md
@@ -10,13 +10,13 @@ Gran turismo sport
 
   - Ao ler e reconhecer o arquivo pelo nome especifico (gtsCarro.json), automaticamente o arquivo será identificado, uma copia do mesmo será feita no diretório ../data/dr2 e o conteúdo do arquivo será processado, gerando um item collection no banco. o arquivo original será deletado do diretório ../data/dr1
 
-A estrutura do diretório de gtSport precisa estar desta forma.
+  - A estrutura do diretório de gtSport precisa estar desta forma.
 
-..
-data
-  dr1
-  dr2
-src
+    ..
+    data
+      dr1
+      dr2
+    src
 
 
 Dependência:
--- src/gtsServer.py
+++ src/gtsServer.py
@@ -3,7 +3,9 @@
 # Neviim - 2017
 #
 from pymongo import MongoClient
+import pymongo
 import datetime
+import hashlib
 import shutil
 import json
 import time
@@ -89,10 +91,39 @@ class GranTurismoSport(object):
         # Abre uma Collection do DataBase GranTurismoSport.
         clientdb = MongoClient(server, port)
         db = clientdb.GranTurismoSport
+
+        # Se não existir o index ele sera criado.
+        #if !exists(db.collection.exists("idkey")) {
+        db.carros.create_index('idkey', unique=True)
+        #}
+
         # Colesão de carros
         self.carros = db.carros
 
-    def cadastra_carro(self, carro):
+    def gera_md5(self, string1, string2):
+        """ recebe duas string e retorna um codigo unico MD5.
+                - Parametro:
+                    string1 = qualquer valor string
+                    string2 = qualquer valor string diferente da primeira
+
+                - Uso:
+                    gtSport = GranTurismoSport()
+                    idkey = gtSport.gera_md5(string1, string2)
+
+                - Retorno:
+                    retorna um valor unico md5 semelhante a este formato,
+                    '18e01c02fac56c795d6eab7a36b08bc5', valor este gerado
+                    pela combinação da string1, string2 e uma key: neviim.
+        """
+        #string1 = "Mustang GT Premium Fastback '15"
+        #string2 = "N400"
+        mpwd = hashlib.md5(string1.encode())
+        apwd = mpwd.hexdigest()
+        codigo  = string2+"neviim"+apwd
+        idcarro = hashlib.md5(codigo.encode())
+        return idcarro.hexdigest()
+
+    def cadastra_carro(self, gts):
         """ cadastra o os dados pasado por parametro no dicionario carro
                 - Parametros:
                     carro => dicionario com os dados do carro a ser cadastrado.
@@ -101,19 +132,34 @@ class GranTurismoSport(object):
                     granturismosport = GranTurismoSport()
                     granturismosport.cadastro_carro(<status_carro_json>)
 
+                - Dependencia:
+                    Obrigatoriamente os campos chaves usado abaxo precisa constar
+                    no arquivo json fornecido, [carro, data, modelo, categoria],
+                    caso ele seja alterado, esta função não funcionara corretamente.
+
         """
-        carro = self.carros.insert_one(carro).inserted_id
-        print ("ID: " + str(carro))
+        id_md5 = self.gera_md5(gts['carro']['data']['modelo'], \
+                               gts['carro']['data']['categoria'])
+        #
+        gts['idkey'] = id_md5
+        # efetua o cadastro somente caso este doc não esteja inserido.
+        #print(self.carros.find({'idkey': id_md5}).count())
+        if self.carros.find({'idkey': id_md5}).count() == 0:
+            id_carro = self.carros.insert_one(gts).inserted_id
+            print ("IDmdb: " + str(id_carro))
+            print ("IDkey: " + str(id_md5))
+            return True
+        return False
 
 
-# inicio da aplicação. 
+# inicio da aplicação.
 if __name__ == "__main__":
     # path dos diretorios origem e destino
     dr1 = "../data/dr1" # origem
     dr2 = "../data/dr2" # destino
     tempo = 10 # tempo de espera em segundos
 
-    # ativa classe de monitoramento de fasta.
+    # ativa classe de monitoramento de pasta.
     monitorandoPasta = MonitoraPasta(dr1, dr2)
 
     # loop infinito.
